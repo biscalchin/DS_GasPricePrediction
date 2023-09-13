13b6556588540d392ef0c53d112229d037b83795
biscalchin <alberto.biscalchin@gmail.com>
2023-09-13 19:01:33 +0200

    v1

---
+++ data_scraper.py
@@ -0,0 +1,115 @@
+# Import the necessary libraries
+import numpy as np
+import yfinance as yf  # For Yahoo Finance data retrieval
+import pandas as pd    # For working with data in DataFrame format
+import datetime as dt  # For working with date and time
+from time import *     # For managing time-related operations
+import warnings        # For silencing future warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+# Defining the resource to monitor - in this case, natural gas futures (NG=F)
+resLabel = "NG=F"
+# Define the object 'NG' using the Ticker method from the Yahoo Finance API
+NG = yf.Ticker(resLabel)
+
+
+# Define the function to create the name of the CSV file in the format: {prefix}_NG_{today}{suffix}.csv
+def get_filename(prefix="", suffix=""):
+    today = dt.date.today()  # Get the current date
+    return f"{prefix}_NG_{today}{suffix}.csv"  # Return the formatted filename
+
+
+# Define the function to write a pandas DataFrame to a CSV file with a specified filename
+def save_to_csv(data, prefix="", suffix=""):
+    data.to_csv(get_filename(prefix, suffix), index=False)
+
+
+# Define a function to get the period from the user with strict input validation
+def get_period():
+    while True:
+        period = input(f"Choose the period in days:\n1. 7d\n2. 30d\n3. 60d\n>")
+        if period == "1" or period == "7d" or period == "7":
+            return "7d"
+        elif period == "2" or period == "30d" or period == "30":
+            return "30d"
+        elif period == "3" or period == "60d" or period == "60":
+            return "60d"
+        else:
+            print(f"Error: Invalid Choice!\n\n")
+
+
+# Define a function to get the data retrieval interval from the user with input validation
+def get_interval():
+    while True:
+        interval = input(f"Choose the interval:\n1. 1m\n2. 5m\n3. 15m\n>")
+        if interval == "1" or interval == "1m" or interval == "1min":
+            return "1m"
+        elif interval == "2" or interval == "5min" or interval == "5m" or interval == "5":
+            return "5m"
+        elif interval == "3" or interval == "15min" or interval == "15m" or interval == "15":
+            return "15m"
+        else:
+            print(f"Error: Invalid Choice!\n\n")
+
+
+# Define the main listener function
+def data_scraper():
+    try:
+        # Get user input for period and interval
+        period = get_period()
+        interval = get_interval()
+        # Retrieve and save historical data based on user input
+        data = NG.history(period=period, interval=interval)
+        # launching the data cleaner
+        data = data_cleaner(data)
+        print("Data cleaning terminated. \n*** Data are now clean ***")
+        sleep(0.8)
+        print(data)
+        sleep(0.8)
+        prefix = f"{interval}_interval"
+        suffix = f"_{period}_period"
+        print(f"Saving data to {get_filename(prefix, suffix)}...")
+        save_to_csv(data, prefix, suffix)
+        print("Operation Succeed!")
+
+    # Handle KeyboardInterrupt to gracefully exit the program
+    except KeyboardInterrupt:
+        print("Task finished successfully")
+    except Exception as e:
+        print("Exception encountered:", e)
+
+
+# Define a function called data_cleaner that takes a parameter called resource
+def data_cleaner(resource):
+    try:
+        print("Cleaning...")
+        sleep(0.5)
+        print("Dropping useless columns...")
+        sleep(0.5)
+        # Drop the columns named Dividends and Stock Splits from resource along the horizontal axis
+        resource = resource.drop(["Dividends", "Stock Splits"], axis=1)
+        print("Operation Succeed!")
+        print("Splitting indexes...")
+        sleep(0.5)
+        # Assign the index of resource to date_time and convert it to a series object
+        date_time = resource.index
+        date_time = date_time.to_series()
+        print("Creating date and time column...")
+        sleep(0.5)
+        # Add two new columns to resource named Date and Hour, with values extracted from date_time
+        resource["Date"] = date_time.dt.date
+        resource["Hour"] = date_time.dt.time
+        print("Operation Succeed!")
+        # Wait for 0.5 seconds and return resource as the output of the function
+        sleep(0.5)
+        return resource
+    # Handle KeyboardInterrupt to gracefully exit the program
+    except KeyboardInterrupt:
+        print("Task finished successfully")
+    # Handle any other exception and print its message
+    except Exception as e:
+        print("Exception encountered:", e)
+
+
+# Entry point of the script
+if __name__ == '__main__':
+    data_scraper()  # Call the main data_scraper function to start data retrieval and processing
---
+++ readme.md
@@ -0,0 +1,41 @@
+# Natural Gas Price Forecasting Model
+
+## Abstract
+
+The goal of this project is to create a natural gas price forecasting model using five distinct regression techniques. Natural gas price, being a continuous variable, will be predicted using supervised regression algorithms. This project will explore and compare the following regression techniques: Linear Regression, Polynomial Regression, Decision Trees Regression, Random Forest Regression, and Artificial Neural Network Regression. The aim is to provide a comprehensive review of natural gas price forecasting methods and evaluate the performance of each algorithm.
+
+## Authors
+
+- Alberto Biscalchin
+- Adnane Soulaimani
+
+## Methodology
+
+In this project, we will investigate five regression algorithms for predicting future natural gas prices:
+
+1. **Linear Regression**: This model assumes a linear relationship between explanatory variables and natural gas prices and will serve as our baseline.
+
+2. **Polynomial Regression**: Extending linear regression, this model introduces nonlinear terms to capture more complex relationships between variables.
+
+3. **Decision Trees Regression**: This algorithm divides the explanatory variable space into homogeneous regions and assigns each region a constant value of the target variable.
+
+4. **Random Forest Regression**: An evolution of decision trees regression, this model combines multiple decision trees into an ensemble model to provide robustness and resist overfitting.
+
+5. **Artificial Neural Network Regression**: This approach employs deep neural networks to capture complex nonlinear relationships between explanatory variables and the price of natural gas.
+
+During the analysis, historical natural gas price data or other relevant variables will be used. The data will be split into a training set and a test set to evaluate the predictive performance of each algorithm.
+
+## Expected Results
+
+We anticipate that the performance of the various regression methods in predicting natural gas prices will vary. Here are our initial expectations:
+
+- **Polynomial Regression**: Preferred for more complex correlations.
+- **Linear Regression**: Beneficial for roughly linear relationships.
+- **Artificial Neural Networks**: Effective at handling extremely nonlinear relationships.
+- **Decision Trees and Random Forest**: Better at capturing oscillations and interactions in the data.
+
+## Preliminary Conclusions
+
+This project represents a valuable step in our journey to study machine learning principles as undergraduate students in the Master of Science in Data Science program. It allows us to apply theoretical insights learned during our courses in a practical setting by studying and comparing five regression models for forecasting natural gas prices. This experience will provide us with a solid foundation for understanding the importance of algorithm selection based on the nature of the data and forecasting targets as we continue to develop our Data Science skills. We are excited about the upcoming academic challenges and learning opportunities.
+
+Feel free to explore the code and results in this repository to gain insights into natural gas price forecasting using various regression techniques.
