0277d43ea2dfaae5478cfaf7055eb5f44a56ca83
biscalchin <alberto.biscalchin@gmail.com>
2023-12-10 19:00:27 +0100

    Completed

--- docs/conf.py
+++ docs/conf.py
@@ -20,11 +20,11 @@ release = '0.9 (Alpha)'
 
 extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ['templates']
+exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_static_path = ['static']
